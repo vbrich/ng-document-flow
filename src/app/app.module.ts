28c873312785596f1a7b75ce170fd5b74ce67537
vbrich <vb_rich@yahoo.com>
2022-05-14 07:45:12 -0700

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppMaterialModule } from './app.material.module';
 import { AppComponent, DialogContentComponent } from './app.component';
+
+import { TestComponent } from './test/test.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     AppMaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      { path: '', component: TestComponent },
+      { path: 'test2', component: Test2Component },
+    ]),
   ],
-  declarations: [AppComponent, DialogContentComponent],
+  declarations: [
+    AppComponent,
+    DialogContentComponent,
+    TestComponent,
+    Test2Component,
+  ],
   entryComponents: [DialogContentComponent],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
