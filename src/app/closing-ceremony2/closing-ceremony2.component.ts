b1e1076bcb69069c074105568b0e402dfeeb3a80
vbrich <vb_rich@yahoo.com>
2022-05-17 06:38:31 -0700

    2 files modified, 1 added

--- src/app/closing-ceremony2/closing-ceremony2.component.ts
+++ src/app/closing-ceremony2/closing-ceremony2.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material';
 
 export interface Document {
   id: number;
@@ -6,8 +7,14 @@ export interface Document {
   desc: string;
 }
 
-const DOCUMENT_DATA: Document[] = [
+const DOCUMENT1: Document[] = [
   {id: 1, name: 'Federal Truth in Lending Disclosure', desc: '4 Signatures 12 Pages'}
+];
+const DOCUMENT2: Document[] = [
+  {id: 2, name: 'Credit Score Summary', desc: '0 Signatures 1 Page'}
+];
+const DOCUMENT3: Document[] = [
+  {id: 3, name: 'Odometer Statement', desc: '0 Signatures 1 Page'}
 ];
 
 @Component({
@@ -16,11 +23,27 @@ const DOCUMENT_DATA: Document[] = [
   styleUrls: ['./closing-ceremony2.component.css']
 })
 export class ClosingCeremony2Component implements OnInit {
-
   displayedColumns: string[] = ['id', 'name'];
-  dataSource = DOCUMENT_DATA;
+  document1 = DOCUMENT1;
+  document2 = DOCUMENT2;
+  document3 = DOCUMENT3;
 
-  constructor() { }
+  //DIALOG
+  title = 'angular-material-dialog-app';
+  @ViewChild('dialogRef')
+  dialogRef!: TemplateRef<any>;
+  myFooList = ['Some Item', 'Item Second', 'Other In Row', 'What to write', 'Blah To Do']
+
+  constructor(public dialog: MatDialog) {
+  }
+
+  openTempDialog() {
+    const myTempDialog = this.dialog.open(this.dialogRef, { data: this.myFooList });
+    myTempDialog.afterClosed().subscribe((res) => {
+      // Data back from dialog
+      console.log({ res });
+    });
+  }
 
   ngOnInit() {
   }
