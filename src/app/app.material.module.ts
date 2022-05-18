56d4ebfbd6874c4e2e84bc2296b7d3bbee6f41dc
vbrich <vb_rich@yahoo.com>
2022-05-18 11:06:38 -0700

    3 files modified

--- src/app/app.material.module.ts
+++ src/app/app.material.module.ts
@@ -23,6 +23,7 @@ import {
   MatListModule,
   MatTableModule,
   MatTooltipModule,
+  MatExpansionModule
 } from '@angular/material';
 
 @NgModule({
@@ -50,6 +51,7 @@ import {
     MatListModule,
     MatTableModule,
     MatTooltipModule,
+    MatExpansionModule
   ],
   exports: [
     MatButtonModule,
@@ -75,6 +77,7 @@ import {
     MatListModule,
     MatTableModule,
     MatTooltipModule,
+    MatExpansionModule
   ],
 })
 export class AppMaterialModule {}
